a06b2cfd987934416b6780ac541cd314f304d996
MAHIMA NAGESHWAR <mahimanageshwar05@gmail.com>
2026-04-08 10:36:13 +0530

    Remove print statements from transposeOfMatrix
    Removed the print statements for the transposed matrix.

--- transposeOfMatrix.java
+++ transposeOfMatrix.java
@@ -11,13 +11,19 @@
                 b[i][j] = a[j][i];
             }
         }
-        //print array
-        for(int i=0; i<b.length; i++)
-        {
-            for(int j=0; j<b[0].length; j++)
-            {
-                System.out.print(b[i][j] +" ");
-            }
+        return b;
+    }
+
+    public static void main(String args[])
+    {
+          int a[][] = {{1,2,3}, {4,5,6}};
+          int ans[][] = transposeOfMatrix(a);  
+          for(int i=0; i<ans.length; i++)
+          {
+              for(int j=0; j<ans[0].length; j++)
+              {
+                 System.out.print(ans[i][j] +" ");
+              }
             System.out.println();
-        }
+        }       
     }
