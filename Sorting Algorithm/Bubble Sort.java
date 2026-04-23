4c5ecfcfdad92d1ab56b7bdc4ffb0577d4b5dd2b
MAHIMA NAGESHWAR <mahimanageshwar05@gmail.com>
2026-04-23 22:11:34 +0530

    update code

--- Sorting Algorithm/Bubble Sort.java
+++ Sorting Algorithm/Bubble Sort.java
@@ -4,14 +4,20 @@ public class Bubble_Sort
     {
         for(int i=0; i<arr.length-1; i++)
         {
+            boolean flag = false;
             for(int j=1; j<arr.length-i-1; j++)
             {
                 if(arr[j] > arr[j+1])
                 {
+                    flag = true;
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
+            }
+            if(flag)
+            {
+                break;
             }
         }
         return arr;
